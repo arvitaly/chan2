4611c2941db96127015aedd2efbe4810efbe9dbb
Alexandro <arvitaly@list.ru>
2017-06-03 07:24:25 +0300

    add generic

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import Chan from "./Chan";
-export function makechan(name?: string) {
-    return new Chan(name);
+export function makechan<T>(name?: string) {
+    return new Chan<T>(name);
 }
 export default makechan;
